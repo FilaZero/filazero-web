d980102f6006bfff4ffcbdb0a7ca7b7e1a1701bb
Erickson <ericksonsilva@live.com>
2014-06-26 09:14:39 -0300

    Implementação dos emails e links do Facebook de 'Quem Somos'

--- js/views/aboutusView.js
+++ js/views/aboutusView.js
@@ -13,16 +13,24 @@ define(['jquery','underscore','backbone','views/compiled-templates'],function ($
 									subtitle: 'FilaZero',
 									developer_one: 'Franco Neto',
 									describe_developer_one: 'Aluno da Universidade Federal da Paraíba, 23 anos.',
+									email_developer_one: 'malito:franco.neto@dce.ufpb.br',
+									facebook_developer_one: 'http://www.facebook.com/franco.neto.75',
 									picture_one: '../bower_components/filazero/assets/franco.jpg',
 									developer_two: 'Renan Soares',
 									describe_developer_two: 'Aluno da Universidade Federal da Paraíba, 22 anos.',
+									email_developer_two: 'malito:renan.soares@dce.ufpb.br',
+									facebook_developer_two: 'http://www.facebook.com/renansoaresdeandrade',
 									picture_two: '../bower_components/filazero/assets/renan.jpg',
 									developer_three: 'Erickson Silva',
 									describe_developer_three: 'Aluno da Universidade Federal da Paraíba, 22 anos.',
+									email_developer_three: 'malito:erickson.oliveira@dce.ufpb.br',
+									facebook_developer_three: 'http://www.facebook.com/ericksonsilva0',
 									picture_three: '../bower_components/filazero/assets/erickson.jpg',
-									developer_for: 'Franclis Galdino',
-									describe_developer_for: 'Aluno da Universidade Federal da Paraíba, 21 anos.',
-									picture_for: '../bower_components/filazero/assets/franclis.jpg'
+									developer_four: 'Franclis Galdino',
+									describe_developer_four: 'Aluno da Universidade Federal da Paraíba, 21 anos.',
+									email_developer_four: 'malito:franclis.silva@dce.ufpb.br',
+									facebook_developer_four: 'http://www.facebook.com/junior.galdino.74',
+									picture_four: '../bower_components/filazero/assets/franclis.jpg'
 									}));
 			return this;
 		}
