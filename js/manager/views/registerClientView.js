6234db6a8e0264fc6fecd675ffbff34a3f22c7c4
FrancoNeto <netom.m@hotmail.com>
2014-07-24 21:08:05 -0300

    Setar Campos caso exista CPF

--- js/manager/views/registerClientView.js
+++ js/manager/views/registerClientView.js
@@ -14,12 +14,23 @@ define(['handlebars','jquery','underscore','backbone','text!manager/templates/re
       this.$Sexo = this.$("#Sexo");
       this.$Email = this.$("#Email");
       this.$Telefone = this.$("#Telefone");
-      this.$("#CPF").blur(function(){
-          alert("O input perdeu o foco.");
-      });
+      this.existCPF(this.$CPF,this.$Nome,this.$Sexo,this.$Email,this.$Telefone);
     },
     events: {
       'click #add-client' : 'addClient'
+    },
+    existCPF:function($CPF,$Nome,$Sexo,$Email,$Telefone) {
+      $CPF.blur(function(){ 
+        ClientsCollection.each(function(model) {
+          if(model.get("CPF") == $CPF.val()){        
+            $Nome.val(model.get("Nome"));
+            $Sexo.val(model.get("Sexo"));
+            $Email.val(model.get("Email"));
+            $Telefone.val(model.get("Telefone"));
+          }
+        });
+      });
+      
     },
     addClient : function(event){
       ClientsCollection.create({CPF: this.$CPF.val(),
