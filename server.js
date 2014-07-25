6fc4aa9140a8b994e1161fb3a749024b9b46e107
Junior Galdino <junior@galdino.(none)>
2014-07-25 09:24:38 -0300

    session add

--- server.js
+++ server.js
@@ -8,20 +8,31 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
 var HOST = '0.0.0.0';
 var PORT = 9999;
 
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var app = express();
+
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({secret: 'secret'}));
 
 
 //Root
 app.get('/', function(req, res){
-  res.send('index.html');
+  res.sendfile('index.html');
 });
+
+
+app.post('/login/adm', loginAdm);
+
 
 //routers clientes
 app.get('/cliente', getUsers);
@@ -32,7 +43,47 @@ app.delete('/cliente/',deleteUsers);
 app.put('/cliente/cpf',updateUser);
 app.put('/cliente',updateUsers);
 
-//functions clientes
+
+//routers doctors
+app.get('/medico', getDoctors);
+app.get('/medico/:crm' , getDoctor);
+app.get('/medico/estab/:idEstab', getDoctorsEstab);
+app.put('/medico/:crm', updateDoctor);
+app.put('/medico', updateDoctors);
+//app.delete('/medico', deleteDoctor);
+//add.delete('/medico/estab', deleteRelationDoctorEstab);
+//app.post('/medico', addDoctor);
+//app.post('/medico/estab',addRelationDoctorEstab);
+
+
+
+//routers estabelishments
+app.get('/estabelecimento', getEstabs);
+app.get('/estabelecimento/:id',getEstab);
+
+function loginAdm(req, res, next) {
+	var query = connection.query('SELECT * FROM tb_administrador WHERE Login = ? AND Senha = ? ', 
+			    [req.body.login, req.body.senha], function(err, rows){
+			    	if(!err){
+			    		if(rows!=null){
+			    			var adm = rows[0];
+			    			req.session.idEstab = adm.FK_Estabelecimento;
+			    			res.send(200, req.session.idEstab);
+			    			console.log(adm);
+			    			console.log(req.session.idEstab);
+			    		}
+			    		else{
+			    			res.send(403,'Administrador nao cadastrado');
+			    		}			    		
+			    	}
+			    });
+}
+
+ 
+
+
+
+// ------------------------------------------------- Function Users --------------------------------------------------------
 function getUsers (req, res) {
   var query = connection.query('SELECT * FROM tb_cliente', function(err, rows, fields) {
     if (!err) res.jsonp(rows);
@@ -54,18 +105,24 @@ function getUser (req,res) {
 }
 
 function addUser (req,res) {
-  var query = connection.query('INSERT INTO tb_cliente (CPF, Nome, Login, Senha, Sexo, Email, Telefone) Values(?,?,?,?,?,?,?)',
-  [req.body.CPF, req.body.Nome, req.body.Login , req.body.Senha , req.body.Sexo , req.body.Email , req.body.Telefone],function(err){
-      if(!err) {
-        res.send(200,'Cliente adicionado'); 
-        console.log('Cliente adicionado');
-      } 
-      else{
-        res.send(403,'Verifique os dados');
-        console.log('Verifique os dados');
-        console.log(err);
-      }          
-  });
+  if(res.session!=null){
+    var query = connection.query('INSERT INTO tb_cliente (CPF, Nome, Login, Senha, Sexo, Email, Telefone) Values(?,?,?,?,?,?,?)',
+    [req.body.CPF, req.body.Nome, req.body.Login , req.body.Senha , req.body.Sexo , req.body.Email , req.body.Telefone],function(err){
+        if(!err) {
+          res.send(200,'Cliente adicionado'); 
+          console.log('Cliente adicionado');
+        } 
+        else{
+          res.send(403,'Verifique os dados');
+          console.log('Verifique os dados');
+          console.log(err);
+        }          
+    });
+  }
+  else{
+    console.log(req.session);
+    res.send(403,'Faca o login');
+  }
 }
 
 function deleteUser (req,res) {
@@ -148,7 +205,124 @@ function updateUsers(req,res){
   });
 }
 
+// ------------------------------------------------- Functions Doctors --------------------------------------------------------
+function getDoctors(req, res){
+  var query = connection.query('SELECT * FROM tb_medico', function(err, rows){
+    if (!err) res.jsonp(rows);
+    else{
+      res.send(403,'Ocorreu algum erro');
+      console.log(err);
+    }
+  });
+}
+
+function getDoctor(req, res){
+  var query = connection.query('SELECT * FROM tb_medico WHERE CRM = ?', req.params.crm, function(err, rows){
+    if (!err) res.jsonp(rows[0]);
+    else{
+      res.send(403,'Ocorreu algum erro');
+      console.log(err);
+    }
+  });
+}
+
+function getDoctorsEstab(req, res){
+	if(!req.session.idEstab){
+	  var query = connection.query('SELECT med.CRM, med.Nome, med.Descricao, med.Especialidade ' +
+	                               'FROM tb_medico AS med, tb_medico_trabalha_estabelecimento AS mestb' +
+	                               'WHERE mestb.FK_Estabelecimento_Med = ? AND med.CRM = mestb.FK_Medico_Estab', req.session.idEstab, function(err, rows){
+	    if (!err) res.jsonp(rows);
+	    else{
+	      res.send(403,'Ocorreu algum erro');
+	      console.log(err);
+	    }
+	  });
+	}  
+}
+
+
+function updateDoctors(req, res){
+  var doctorTemp = req.body;
+  var queryTemp = 'UPDATE tb_medico SET '
+  var count = 0
+  for(var key in doctorTemp) queryTemp+= key +' = \"' + doctorTemp[key] + '\" ,';       
+  
+  var temp = new String(queryTemp); 
+  queryTemp = temp.substring(0,(temp.length-1)); //retira a ultima virgula
+
+  var query = connection.query(queryTemp,function(err){
+    if(!err){
+      res.send(200,'Medicos atualizados');
+      console.log('Medicoss atualizados');
+    }
+    else {
+      res.send(403,'Erro ao atualizar medicos');
+      console.log('Erro ao atualizar medicos');
+      console.log(err);
+    } 
+  }); 
+}
+
+function updateDoctor(req, res){
+  var doctorTemp = req.body;
+  var queryTemp = 'UPDATE tb_medico SET '
+  var count = 0
+  for(var key in doctorTemp){
+    if(count>0) queryTemp+= key +' = \"' + doctorTemp[key] + '\" ,';       
+    count++;
+  } 
+  var temp = new String(queryTemp); 
+  queryTemp = temp.substring(0,(temp.length-1)); //retira a ultima virgula
+  queryTemp+= ' WHERE CRM = ?';
+  var crm = doctorTemp.CRM;
+  
+  var query = connection.query(queryTemp, crm, function(err){
+    if(!err){
+      res.send(200,'Medico atualizado');
+      console.log('Medico atualizado');
+    }else {
+      res.send(403,'Erro ao atualizar medico');
+      console.log('Erro ao atualizar medico');
+      console.log(err);
+    }  
+  });
+}
+
+
+// ------------------------------------------------- Functions Estabs --------------------------------------------------------
+
+function getEstabs(req, res){
+  var query = connection.query('SELECT est.CNES, est.Nome, est.Descricao, tel.Telefone, est.Numero, end.Estado, end.Cidade, end.Bairro, end.Rua '+
+                               'FROM tb_estabelecimento AS est, tb_estabelecimento_endereco AS estend, ' +
+                                     'tb_endereco AS end, tb_telefone_estabelecimento AS tel ' +
+                                'WHERE est.CNES = estend.FK_Estabelecimento_End AND ' + 
+                                      'end.PK_Endereco = estend.FK_Endereco_Estab AND tel.FK_Estabelecimento = est.CNES ', 
+                                function(err, rows, fields) {
+    if (!err) res.jsonp(rows);
+    else{
+      res.send(403,'Ocorreu algum erro')
+      console.log(err);
+    }
+  });
+}  
+
+function getEstab(req, res){
+  var query = connection.query('SELECT est.CNES, est.Nome, est.Descricao, tel.Telefone, est.Numero, end.Estado, end.Cidade, end.Bairro, end.Rua '+
+                               'FROM tb_estabelecimento AS est, tb_estabelecimento_endereco AS estend, ' +
+                                    'tb_endereco AS end, tb_telefone_estabelecimento AS tel ' +
+                               'WHERE est.CNES = ? AND est.CNES = estend.FK_Estabelecimento_End AND ' + 
+                                    'end.PK_Endereco = estend.FK_Endereco_Estab AND tel.FK_Estabelecimento = est.CNES ',req.params.id,
+                               function(err, rows, fields) {
+    if (!err) res.jsonp(rows[0]);
+    else{
+      res.send(403,'Ocorreu algum erro')
+      console.log(err);
+    }
+  });
+}
+
+
 
 var server = app.listen(PORT,HOST, function() {
-    console.log('Servidor rodando em'+HOST + ':' + PORT);
+    console.log('Servidor rodando em: '+HOST + ':' + PORT);
 });
