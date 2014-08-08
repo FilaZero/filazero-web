7bc4c44037de035fb92595138d52ae8a24c5e17f
FrancoNeto <netom.m@hotmail.com>
2014-08-07 22:01:37 -0300

    Listar Pacientes

--- js/manager/collections/clientCollection.js
+++ js/manager/collections/clientCollection.js
@@ -5,7 +5,7 @@ define([
 ], function(_, Backbone, Client) {
     var ClientsCollection = Backbone.Collection.extend({
         model: Client,
-        url: "cliente" // url of the REST API
+        url: "manager/paciente" // url of the REST API
     });
     return new ClientsCollection();
 });
--- js/manager/views/listClientView.js
+++ js/manager/views/listClientView.js
@@ -3,25 +3,49 @@ define(['handlebars','jquery','underscore','backbone','text!manager/templates/li
   var listClientView = Backbone.View.extend({
     el:'#content',
     template: Handlebars.compile(listClientTemplate),
+
     initialize:function() {
-      ClientsCollection.fetch();
-      console.log('Initializing ListClient View'); 
       this.render();
+      console.log('Initializing ListClient View');
 
       //Dom
       this.$tbody = this.$('#mytable #tbody');
       this.setClients(this.$tbody);
     },
+    events: {
+      'click #btn-delete': 'modalDelete',
+      'click #btn-confirm': 'deleteConfirm'
+    },
+    modalDelete:function(e){
+      this.$("#delete").modal();
+      this.$("#btn-confirm").attr('clientId',e.currentTarget.attributes[1].value); 
+    },
+    deleteConfirm:function(e) {
+      e.preventDefault();
+      var idForDelete =  this.$("#btn-confirm").attr('clientId');
+      var modelDelete = ClientsCollection.get(idForDelete);
+      modelDelete.set({id: modelDelete.get("CPF")});
+      console.log(modelDelete.get("id"));
+      modelDelete.destroy({
+        success: function(model, response) {
+          console.log("Sucess");
+        },
+        error: function(model, response) {
+          console.log("Erro");
+        }
+      });
+      this.$("#delete").modal("hide");
+    },
     setClients:function($tbody){
         ClientsCollection.each(function(model) {
-          $tbody.append('<tr>')
+          $tbody.append('<tr>');
           $tbody.append('<td>'+model.get("CPF")+ '</td>');
           $tbody.append('<td>'+model.get("Nome")+ '</td>');
           $tbody.append('<td>'+model.get("Sexo")+ '</td>');
           $tbody.append('<td>'+model.get("Email")+ '</td>');
           $tbody.append('<td>'+model.get("Telefone")+ '</td>');
-          $tbody.append('<td><p><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" data-placement="top" rel="tooltip"><span class="glyphicon glyphicon-pencil"></span></button></p></td>');
-          $tbody.append(' <td><p><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" data-placement="top" rel="tooltip"><span class="glyphicon glyphicon-trash"></span></button></p></td>');
+          $tbody.append('<td><p><button class="btn btn-primary btn-xs" data-title="Edit"><span class="glyphicon glyphicon-pencil"></span></button></p></td>');
+          $tbody.append('<td><p><button id="btn-delete"  idClient='+model.cid+' class="btn btn-danger btn-xs" data-title="Delete"><span class="glyphicon glyphicon-trash"></span></button></p></td>');
           $tbody.append('</tr>');
         });  
     },
